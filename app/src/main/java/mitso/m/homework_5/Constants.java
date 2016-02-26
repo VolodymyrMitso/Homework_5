67dd6d4b9c4cd7ebfdcb385a823d975606f7e440
Volodymyr Mitso <volodymyrmitso@gmail.com>
2016-02-26 22:10:42 +0200

    emails sending works ...

---
+++ app/src/main/java/mitso/m/homework_5/Constants.java
@@ -0,0 +1,7 @@
+package mitso.m.homework_5;
+
+public class Constants {
+
+    public static final String TO = "To:";
+    public static final String SUBJECT = "Subject:";
+}
--- app/src/main/java/mitso/m/homework_5/MainActivity.java
+++ app/src/main/java/mitso/m/homework_5/MainActivity.java
@@ -1,25 +1,47 @@
 package mitso.m.homework_5;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mButton_Send;
+    EditText mEditText_To;
+    EditText mEditText_Subject;
+    EditText mEditText_MessageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.tb_Toolbar_AM);
-        setSupportActionBar(myToolbar);
 
-        EditText mEditText_SentTo = (EditText)findViewById(R.id.et_SendTo_AM);
-        TextDrawable.drawStringInEditText(mEditText_SentTo, "Send To:", 250);
+        mEditText_To = (EditText) findViewById(R.id.et_To_AM);
+        TextDrawable.drawStringInEditText(mEditText_To, Constants.TO, 110);
+        mEditText_Subject = (EditText) findViewById(R.id.et_Subject_AM);
+        TextDrawable.drawStringInEditText(mEditText_Subject, Constants.SUBJECT, 235);
 
-        EditText mEditText_Subject = (EditText)findViewById(R.id.et_Subject_AM);
-        TextDrawable.drawStringInEditText(mEditText_Subject, "Subject:", 235);
+        mEditText_MessageText = (EditText) findViewById(R.id.et_MessageText_AM);
+        mButton_Send = (Button) findViewById(R.id.btn_Send_AM);
 
+        mButton_Send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String To = mEditText_To.getText().toString();
+                String subject = mEditText_Subject.getText().toString();
+                String messageText = mEditText_MessageText.getText().toString();
 
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[] {To});
+                email.putExtra(Intent.EXTRA_SUBJECT, subject);
+                email.putExtra(Intent.EXTRA_TEXT, messageText);
+                email.setType("message/rfc822");
+
+                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+            }
+        });
     }
 }
