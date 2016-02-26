63c9b9907a5038917905ba6da141b6b136aeda3a
Volodymyr Mitso <volodymyrmitso@gmail.com>
2016-02-26 23:35:37 +0200

    something

--- app/src/main/java/mitso/m/homework_5/Constants.java
+++ app/src/main/java/mitso/m/homework_5/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
 
     public static final String TO = "To:";
     public static final String SUBJECT = "Subject:";
+
+    public static final String TEL = "tel:+380995339692";
 }
--- app/src/main/java/mitso/m/homework_5/MainActivity.java
+++ app/src/main/java/mitso/m/homework_5/MainActivity.java
@@ -1,6 +1,8 @@
 package mitso.m.homework_5;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,10 +11,11 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button mButton_Send;
     EditText mEditText_To;
     EditText mEditText_Subject;
     EditText mEditText_MessageText;
+    Button mButton_Send;
+    Button mButton_Call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +23,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main);
 
         mEditText_To = (EditText) findViewById(R.id.et_To_AM);
-        TextDrawable.drawStringInEditText(mEditText_To, Constants.TO, 110);
         mEditText_Subject = (EditText) findViewById(R.id.et_Subject_AM);
+        mEditText_MessageText = (EditText) findViewById(R.id.et_MessageText_AM);
+
+        TextDrawable.drawStringInEditText(mEditText_To, Constants.TO, 110);
         TextDrawable.drawStringInEditText(mEditText_Subject, Constants.SUBJECT, 235);
 
-        mEditText_MessageText = (EditText) findViewById(R.id.et_MessageText_AM);
         mButton_Send = (Button) findViewById(R.id.btn_Send_AM);
-
         mButton_Send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,13 +38,30 @@ public class MainActivity extends AppCompatActivity {
                 String messageText = mEditText_MessageText.getText().toString();
 
                 Intent email = new Intent(Intent.ACTION_SEND);
-                email.putExtra(Intent.EXTRA_EMAIL, new String[] {To});
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{To});
                 email.putExtra(Intent.EXTRA_SUBJECT, subject);
                 email.putExtra(Intent.EXTRA_TEXT, messageText);
                 email.setType("message/rfc822");
 
-                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+                startActivity(Intent.createChooser(email, "Choose an Email client:"));
             }
         });
+
+        mButton_Call = (Button) findViewById(R.id.btn_Call_AM);
+        mButton_Call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent call = new Intent(Intent.ACTION_CALL);
+                call.setData(Uri.parse(Constants.TEL));
+                if (checkWriteExternalPermission())
+                    startActivity(call);
+            }
+        });
+    }
+
+    private boolean checkWriteExternalPermission() {
+        String permission = "android.permission.CALL_PHONE";
+        int res = getApplicationContext().checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
     }
 }
